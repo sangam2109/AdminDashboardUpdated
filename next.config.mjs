919e4da27e40fe5632063bbf8ec60ba4fb92e9ab
Sangam Arora <arorasam2109@gmail.com>
2024-06-23 16:23:16 +0530

    changes did in config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,26 @@
+// Import necessary modules
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 // Manually define __dirname
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default {
+// Define the Next.js configuration
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     sassOptions: {
         includePaths: [join(__dirname, 'styles')],
     },
 };
+
+export default nextConfig;
